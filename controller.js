fa48307a5c1b1f6fae34dc6e61c8790db514984c
shl3vi <shaharl@wix.com>
2017-07-24 12:50:21 +0300

    move view related code to view.js

--- controller.js
+++ controller.js
@@ -4,89 +4,39 @@
   window.Stokr = window.Stokr || {};
 
   const Model = window.Stokr.Model;
-  // const View = window.Stokr.View;
-
-  const DAILY_CHANGE_STATE_PERCENTAGE = "percentage";
-  const DAILY_CHANGE_STATE_VALUE = "value";
-
-  let dailyChangeState = DAILY_CHANGE_STATE_PERCENTAGE;
+  const View = window.Stokr.View;
 
   function renderFirstTime() {
 
     Model.initStocksDisplayOrder();
     Model.dailyChangeState = Model.dailyChangeStates.DAILY_CHANGE_STATE_PERCENTAGE;
 
-    const stocksListPageTemplate = `
-<div class="Stocks-List-Page">
-  <div class="content-container">
-  <header>
-  <div class="stokr-logo-container">
-  <span>stokr</span>
-  </div>
-  <div class="functional-buttons-container">
-  ${createFunctionalButtons()}
-  </div>
-  </header>
-  <main>
-  <ul class="stocks-list">${createStockListItems()}</ul>
-  </main>
-  </div>
-  </div>
-`;
+    const stockListItems = getOrderedStocks();
+    View.renderFirstTime(stockListItems);
 
-    document.querySelector('body').innerHTML = stocksListPageTemplate;
-    addEvents();
+    View.addEvents();
   }
 
-  function createFunctionalButtons() {
-    return `<div><button class="icon-search"></button></div>
-  <div><button class="icon-refresh"></button></div>
-  <div><button class="icon-a"></button></div>
-  <div><button class="icon-settings"></button></div>`;
+  function getOrderedStocks() {
+    const orderList = Model.stocksDisplayOrder;
+    const orderedStocks = [];
+    orderList.forEach((symbol) => {
+      let stock = Model.getStockBySymbol(symbol);
+      stock.dailyChange = getDailyChangeButtonValue(stock);
+      orderedStocks.push(stock);
+    });
+
+    return orderedStocks;
   }
 
-  function createStockListItems() {
-    const len = Model.getStocksSize();
-    let liHtml = '';
-    for (let i = 0; i < len; i++) {
-      liHtml += createStockListItem(Model.getStockByIndex(i));
-    }
-    return liHtml;
+  function reorderArrowClickUpHandler(symbol){
+    Model.reorderStocksUp(symbol);
+    View.renderPage(getOrderedStocks());
   }
 
-  function createStockListItem(stockItem) {
-    const stockLi = `
-  <li data-id="${stockItem.Symbol}">
-  <div class="line-container">
-  <span class="comp-name">${stockItem.Name}</span>
-<div class="line-container-right">
-  <div class="comp-stock-price-container">
-  <span class="comp-stock-price">${utils.toFixed(stockItem.LastTradePriceOnly)}</span>
-  </div>
-  <div class="daily-change-container">
-  <button ${getDailyChangeButtonClass(stockItem)}>${getDailyChangeButtonValue(stockItem)}</button>
-  </div>
-  <div class="up-down-buttons-container">
-  <div class="up-down-buttons">
-  <button class="reorder-arrow reorder-up"></button>
-  <button class="reorder-arrow reorder-down"></button>
-  </div>
-  </div>
-  </div>
-  </div>
-  </li>`;
-
-    return stockLi;
-  }
-
-
-  function getDailyChangeButtonClass(stockItem) {
-    let style = "daily-change-positive";
-    if (stockItem.Change < 0) {
-      style = "daily-change-negative";
-    }
-
-    return `class="daily-change ${style}"`;
+  function reorderArrowClickDownHandler(symbol){
+    Model.reorderStocksDown(symbol);
+    View.renderPage(getOrderedStocks());
   }
 
   function getDailyChangeButtonValue(stockItem) {
@@ -97,91 +47,35 @@
     }
   }
 
-  function renderPage() {
-    STOCKS_UL.innerHTML = createStockListItems();
+  function getStockBySymbol(symbol) {
+    return Model.getStockBySymbol(symbol);
   }
 
-  function reorderArrowClickHandler(arrow) {
-    let parentLi = getParentByTag(arrow, 'LI');
-    if (arrow.className.includes('reorder-up')) {
-      Model.reorderStocksUp(parentLi.getAttribute('data-id'));
-    } else if (arrow.className.includes('reorder-down')) {
-      Model.reorderStocksDown(parentLi.getAttribute('data-id'));
-    }
-
-    STOCKS_UL.innerHTML = createStockListItems();
-  }
-
-  function addEvents() {
-    document.querySelector('.stocks-list').addEventListener('click', (e) => {
-      const clickedElement = e.target;
-      if (clickedElement.className.includes('daily-change')) {
-        dailyChangeButtonClickHandler(clickedElement);
-      }
-      else if (clickedElement.className.includes('reorder-arrow')) {
-        reorderArrowClickHandler(clickedElement);
-      }
-    });
-
-    // document.querySelector('.up-down-buttons button:first-child').addEventListener('click', (e) => {
-    //   // go up;
-    //   console.log("Hello");
-    //   const clickedElement = e.target;
-    //
-    //   let liParent = getParentByTag(clickedElement, 'LI');
-    //   console.log(liParent);
-    //   console.log(liParent.previousSibling);
-    //   if (liParent.previousSibling) {
-    //     liParent.parentNode.insertBefore(liParent, liParent.previousSibling);
-    //   }
-    // });
-
-    // document.querySelector('.up-down-buttons button:last-child').addEventListener('click', (e) => {
-    //   const clickedElement = e.target;
-    // });
-  }
-
-  function dailyChangeButtonClickHandler(btn) {
+  function toggleDailyChangeState() {
     Model.toggleDailyChangeState();
-
-    let ulParent = getParentByTag(btn, 'UL');
-    let childLis = ulParent.querySelectorAll('li');
-    childLis.forEach((li) => {
-      changeLiDailyChange(li);
-    });
   }
 
-  function changeLiDailyChange(li) {
-    let symbol = li.getAttribute('data-id');
-
-    let btn = li.querySelector('button');
-
-    let elementData = Model.getStockBySymbol(symbol);
-
-    btn.innerHTML = getDailyChangeButtonValue(elementData);
+  window.Stokr.Controller = {
+    renderFirstTime,
+    getOrderedStocks,
+    getDailyChangeButtonValue,
+    getStockBySymbol,
+    reorderArrowClickDownHandler,
+    reorderArrowClickUpHandler,
+    toggleDailyChangeState
   }
+})();
 
-  function getParentByTag(element, tag) {
-    let parent = element.parentElement;
-    while (parent) {
-      if (parent.tagName === tag) {
-        return parent;
-      }
-      parent = parent.parentElement;
-    }
-  }
+const Controller = window.Stokr.Controller;
 
-  /* Start page rendering flow */
+/* Start page rendering flow */
 
-  renderFirstTime();
+Controller.renderFirstTime();
 
-  const STOCKS_UL = document.querySelector('.stocks-list');
-
-  const FIVE_MIN_IN_MILI = 1080000;
-  setInterval(() => {
-    const stocksUL = document.querySelector('.stocks-list');
-    renderPage(stocksUL);
-  }, FIVE_MIN_IN_MILI);
+const FIVE_MIN_IN_MILI = 1080000;
+setInterval(() => {
+  View.renderPage(Controller.getOrderedStocks());
+}, FIVE_MIN_IN_MILI);
 
 // Test Rendering..
 // setInterval(()=> {
@@ -192,4 +86,3 @@
 //   stocks[0].Name = Date.now();
 // }, 1000);
 
-})();
---
+++ view.js
@@ -0,0 +1,134 @@
+(function () {
+  'use strict';
+
+  window.Stokr = window.Stokr || {};
+
+  function renderFirstTime(stockListItems) {
+    const stocksListPageTemplate = `
+<div class="Stocks-List-Page">
+  <div class="content-container">
+  <header>
+  <div class="stokr-logo-container">
+  <span>stokr</span>
+  </div>
+  <div class="functional-buttons-container">
+  ${createFunctionalButtons()}
+  </div>
+  </header>
+  <main>
+  <ul class="stocks-list">${createStockListItems(stockListItems)}</ul>
+  </main>
+  </div>
+  </div>
+`;
+
+    document.querySelector('body').innerHTML = stocksListPageTemplate;
+
+  }
+
+  function createStockListItems(stockListItems) {
+    let liHtml = '';
+    stockListItems.forEach((stock) => {
+      liHtml += createStockListItem(stock);
+    });
+    return liHtml;
+  }
+
+  function createStockListItem(stockItem) {
+    const stockLi = `
+  <li data-id="${stockItem.Symbol}">
+  <div class="line-container">
+  <span class="comp-name">${stockItem.Name}</span>
+<div class="line-container-right">
+  <div class="comp-stock-price-container">
+  <span class="comp-stock-price">${utils.toFixed(stockItem.LastTradePriceOnly)}</span>
+  </div>
+  <div class="daily-change-container">
+  <button ${getDailyChangeButtonClass(stockItem)}>${stockItem.dailyChange}</button>
+  </div>
+  <div class="up-down-buttons-container">
+  <div class="up-down-buttons">
+  <button class="reorder-arrow reorder-up"></button>
+  <button class="reorder-arrow reorder-down"></button>
+  </div>
+  </div>
+  </div>
+  </div>
+  </li>`;
+
+    return stockLi;
+  }
+
+  function getDailyChangeButtonClass(stockItem) {
+    let style = "daily-change-positive";
+    if (stockItem.Change < 0) {
+      style = "daily-change-negative";
+    }
+
+    return `class="daily-change ${style}"`;
+  }
+
+  function createFunctionalButtons() {
+    return `<div><button class="icon-search"></button></div>
+  <div><button class="icon-refresh"></button></div>
+  <div><button class="icon-a"></button></div>
+  <div><button class="icon-settings"></button></div>`;
+  }
+
+  function addEvents() {
+    document.querySelector('.stocks-list').addEventListener('click', (e) => {
+      const clickedElement = e.target;
+      if (clickedElement.className.includes('daily-change')) {
+        dailyChangeButtonClickHandler(clickedElement);
+      }
+      else if (clickedElement.className.includes('reorder-arrow')) {
+        reorderArrowClickHandler(clickedElement);
+      }
+    });
+  }
+
+  function dailyChangeButtonClickHandler(btn) {
+    const Controller = window.Stokr.Controller;
+    Controller.toggleDailyChangeState();
+
+    let ulParent = utils.getParentByTag(btn, 'UL');
+    let childLis = ulParent.querySelectorAll('li');
+    childLis.forEach((li) => {
+      changeLiDailyChange(li);
+    });
+  }
+
+  function changeLiDailyChange(li) {
+    const Controller = window.Stokr.Controller;
+    let symbol = li.getAttribute('data-id');
+
+    let btn = li.querySelector('button');
+
+    let elementData = Controller.getStockBySymbol(symbol);
+
+    btn.innerHTML = Controller.getDailyChangeButtonValue(elementData);
+  }
+
+  function renderPage(stockListItems) {
+    const STOCKS_UL = document.querySelector('.stocks-list');
+    STOCKS_UL.innerHTML = createStockListItems(stockListItems);
+  }
+
+  function reorderArrowClickHandler(arrow) {
+    const Controller = window.Stokr.Controller;
+    let parentLi = utils.getParentByTag(arrow, 'LI');
+    const symbol = parentLi.getAttribute('data-id');
+    if (arrow.className.includes('reorder-up')) {
+      Controller.reorderArrowClickUpHandler(symbol);
+    } else if (arrow.className.includes('reorder-down')) {
+      Controller.reorderArrowClickDownHandler(symbol);
+    }
+  }
+
+  window.Stokr.View = {
+    renderFirstTime,
+    renderPage,
+    addEvents
+  }
+
+})();
